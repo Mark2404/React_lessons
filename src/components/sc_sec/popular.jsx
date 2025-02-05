bf1d89d23e0365705f8993643165e314a87379f6
User <markpomidorchik@gamil.com>
2025-02-05 17:47:39 +0500

    cards

--- src/components/sc_sec/popular.jsx
+++ src/components/sc_sec/popular.jsx
@@ -32,6 +32,108 @@ export class popular extends Component {
                         </div>
 
                     </div>
+                    <div className="card">
+                        <div className='card_top'>
+                            <img src="./assets/imgs/figma.svg" alt="Figma Logo" className="logo" />
+                            <div>
+
+                                <h2>FIGMA</h2>
+                                <span className="free">Free</span>
+                            </div>
+                        </div>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <div className="icons">
+                            <img src="./assets/imgs/heart.svg" alt="" />
+                            <img src="./assets/imgs/folder.svg" alt="" />
+                            <a href="#" className="visit">
+                                Visit
+                            </a>
+                        </div>
+
+                    </div>
+                    <div className="card">
+                        <div className='card_top'>
+                            <img src="./assets/imgs/figma.svg" alt="Figma Logo" className="logo" />
+                            <div>
+
+                                <h2>FIGMA</h2>
+                                <span className="free">Free</span>
+                            </div>
+                        </div>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <div className="icons">
+                            <img src="./assets/imgs/heart.svg" alt="" />
+                            <img src="./assets/imgs/folder.svg" alt="" />
+                            <a href="#" className="visit">
+                                Visit
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+                <div className='cards_sec'>
+                    <div className="card">
+                        <div className='card_top'>
+                            <img src="./assets/imgs/figma.svg" alt="Figma Logo" className="logo" />
+                            <div>
+
+                                <h2>FIGMA</h2>
+                                <span className="free">Free</span>
+                            </div>
+                        </div>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <div className="icons">
+                            <img src="./assets/imgs/heart.svg" alt="" />
+                            <img src="./assets/imgs/folder.svg" alt="" />
+                            <a href="#" className="visit">
+                                Visit
+                            </a>
+                        </div>
+
+                    </div>
+                    <div className="card">
+                        <div className='card_top'>
+                            <img src="./assets/imgs/figma.svg" alt="Figma Logo" className="logo" />
+                            <div>
+
+                                <h2>FIGMA</h2>
+                                <span className="free">Free</span>
+                            </div>
+                        </div>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <div className="icons">
+                            <img src="./assets/imgs/heart.svg" alt="" />
+                            <img src="./assets/imgs/folder.svg" alt="" />
+                            <a href="#" className="visit">
+                                Visit
+                            </a>
+                        </div>
+
+                    </div>
+                    <div className="card">
+                        <div className='card_top'>
+                            <img src="./assets/imgs/figma.svg" alt="Figma Logo" className="logo" />
+                            <div>
+
+                                <h2>FIGMA</h2>
+                                <span className="free">Free</span>
+                            </div>
+                        </div>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <div className="icons">
+                            <img src="./assets/imgs/heart.svg" alt="" />
+                            <img src="./assets/imgs/folder.svg" alt="" />
+                            <a href="#" className="visit">
+                                Visit
+                            </a>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         )
