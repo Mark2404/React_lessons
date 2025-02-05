a2d3a2918ee057618e1e360cfa768b73b83dabc5
User <markpomidorchik@gamil.com>
2025-02-05 17:39:23 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/fr_sec/header";
+import Popular from "./components/sc_sec/popular";
+function App() {
+  return (
+    <div>
+      < Header />
+      < Popular />
+    </div>
+
+
+
+
+  );
+}
+
+export default App;
---
+++ src/components/sc_sec/popular.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import "./popular.css";
+export class popular extends Component {
+    render() {
+        return (
+            <div className='Tools_sec container'>
+                <div className='title'>
+                    <h1>Most Popular Tools</h1>
+                    <p>Tools for the best Designers and Developers
+                        most popularly used in the world    </p>
+
+
+                </div>
+                <div className='cards_sec'>
+                    <div className="card">
+                        <div className='card_top'>
+                            <img src="./assets/imgs/figma.svg" alt="Figma Logo" className="logo" />
+                            <div>
+
+                                <h2>FIGMA</h2>
+                                <span className="free">Free</span>
+                            </div>
+                        </div>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <div className="icons">
+                            <img src="./assets/imgs/heart.svg" alt="" />
+                            <img src="./assets/imgs/folder.svg" alt="" />
+                            <a href="#" className="visit">
+                                Visit
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default popular
