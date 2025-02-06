56653193005651ed2d953511b71e571f28ee49c0
User <markpomidorchik@gamil.com>
2025-02-06 17:57:53 +0500

    brat

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/fr_sec/header";
 import Popular from "./components/sc_sec/popular";
 import Brend from "./components/third_sec/brands";
 import Tools from "./components/fourth_sec/Tools";
+import Brat from "./components/brat_sec/brat";
 import "./App.css";
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       < Popular />
       < Brend />
       <Tools />
+      <Brat />
     </div>
 
 
---
+++ src/components/brat_sec/brat.jsx
@@ -0,0 +1,26 @@
+import "./brat.css"
+import React from 'react'
+
+const brat = () => {
+    return (
+        <div className='brat_sec container'>
+            <div className='brat-box'>
+                <div className="brat">
+
+                    <img src="./assets/imgs/brat.svg" alt="" />
+                    <div className='brat-text'>
+                        <h3>Ronald Richards</h3>
+                        <p>Product Manager</p>
+                    </div>
+                </div>
+            </div>
+            <div className="brat-description">
+                <img src="./assets/imgs/ds-bg.svg" alt="" />
+                <p>Incididunt cillum do sint sint enim ullamco id deserunt mollit qui reprehenderit do. Velit ex tempor cillum ad sint occaecat. Do nulla velit labore occaecat do deserunt Lorem magna officia incididunt consectetur amet. Sunt consectetur veniam minim ex commodo sint non. Occaecat aute officia excepteur non laboris id qui ad.</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default brat
